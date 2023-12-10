ef1e5fd4dffa7026fb9ffcc436591f990b464b4a
PyramidHead01 <hugo4444333@gmail.com>
2023-12-10 02:05:42 +0100

    2023.12.10-Optimizado un poco el codigo

--- 03_narrative_pyramid/Scripts/Main.gd
+++ 03_narrative_pyramid/Scripts/Main.gd
@@ -1,3 +1,3 @@
 extends Node
 
-	
+@export var idioma = 0
--- 03_narrative_pyramid/Scripts/Player.gd
+++ 03_narrative_pyramid/Scripts/Player.gd
@@ -14,6 +14,7 @@ var ruta_csv
 #Cogemos la ruta nodo de interfaz Dialogo
 var ui_dialogo
 
+
 func _ready():
 	#Obtenemos un array con la resolucion total de la ventana
 	lim = get_viewport_rect().size
@@ -40,29 +41,19 @@ func _process(delta):
 	#Como el Time.deltaTime en unity, para que funcione igual en distintos ordenadores
 	position += mov*delta
 
+	#Si estamos dentor de una colision, entonces podremos si pulsamos espacio empezar el dialogo
 	if interaccion && Input.is_action_just_pressed("ui_select") && !ui_dialogo.dialogo_activo:
 		ui_dialogo.dialogo_activo = true
-		print("Hablando")
-		hablar(ruta_csv)
+		ui_dialogo.pre_dialogo(ruta_csv,get_owner().idioma)
 
-func hablar(ruta):
-
-	print(ruta)
-	
-	print(str(ui_dialogo))
-
-	ui_dialogo.pre_dialogo(ruta,2)
-
-#Es una funcion integrada, seria como un on collision enter
+#Un collision enter, lo mas destacable es que al entrar, hacemos que coja la ruta del CSV
+#tambien decir que unicamente interactua con aquellos grupos que sean "Dialogo"
 func _on_body_entered(body):
 	if body.is_in_group("Dialogo"):
 		$IconoInteraccion.show()
 		interaccion = true
 		ruta_csv = body.ruta_csv
-		print("Dentro")
-
 func _on_body_exited(body):
 	if body.is_in_group("Dialogo"):
 		$IconoInteraccion.hide()
 		interaccion = false
-		print("Fuera")
